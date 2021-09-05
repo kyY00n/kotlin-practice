5f8816e74109d60839a26916df76d5f978e723e7
kayoung yoon <rkdkdudjd@gmail.com>
2021-09-06 00:58:55 +0900

    init 블록 나누어 쓰기

--- src/main/kotlin/SeperatedInitBlocks.kt
+++ src/main/kotlin/SeperatedInitBlocks.kt
@@ -1,2 +1,21 @@
-class SeperatedInitBlocks {
+class SeperatedInitBlocks
+
+fun main(args: Array<String>) {
+    val size = Size(10, 50)
+    println(size.area)
+}
+
+class Size(width: Int, height: Int) {
+    val width = width // 생성자의 매개변수는 1. 프로퍼티를 선언과 동시에 초기화하는 데에도 사용할 수 있다.
+    val height: Int // init 블록에서 값을 할당해주면 에러가 나지 않는다.
+
+    init {
+        this.height = height // 생성자의 매개변수는 2. init블록에서 쓰일 수 있다.
+    }
+
+    val area: Int
+
+    init { // 생성자 블록은 여러개로 나뉠 수 있다.
+        area = width * height
+    }
 }
