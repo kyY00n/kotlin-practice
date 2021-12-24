42b10db23413b931e866d847f71ae35a45a32666
kayoung yoon <rkdkdudjd@gmail.com>
2021-12-25 06:41:17 +0900

    with로 bufferedReader 사용하기

--- src/main/kotlin/BOJ.kt
+++ src/main/kotlin/BOJ.kt
@@ -1,6 +1,9 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
 import java.util.*
 
 class BOJ
-fun main(args: Array<String>) {
-
+fun main(args: Array<String>) = with(BufferedReader(InputStreamReader(System.`in`))) {
+    val int = readLine().toInt()
+    println(int+int)
 }
